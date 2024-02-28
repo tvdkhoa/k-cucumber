e6e5851369221af431ba6dbf90f2f852bf8851a6
tvdkhoa <89145110+tvdkhoa@users.noreply.github.com>
2024-02-28 21:51:32 +0700

    Update RunCucumberTests.java
    remove listenner

--- src/test/java/khoatester/com/runners/RunCucumberTests.java
+++ src/test/java/khoatester/com/runners/RunCucumberTests.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 @CucumberOptions(
         features = "src/test/resources/features/Categories/Categories.feature",
         glue = {"khoatester.com.steps","khoatester.com.hooks"},
-        plugin = {"pretty", "html:target/cucumber-html-report.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","khoatester.com.hooks.CucumberListener"}
+        plugin = {"pretty", "html:target/cucumber-html-report.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 @Test
 public class RunCucumberTests extends AbstractTestNGCucumberTests {
